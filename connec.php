fa9cac37705875fa7a199ebbcaee325d4a652fe3
maxlestage <56438697+maxlestage@users.noreply.github.com>
2019-10-16 11:06:14 +0200

    Delete connec.php

--- connec.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=authentikscooter', 'maxlestage', '18081995Ml');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
